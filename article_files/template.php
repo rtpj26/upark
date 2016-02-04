ae497b9491e7ad8b7cfd2b19a6227284d3b56bcb
Ronilo Penero Jr <ronilo@akubo.net>
2016-02-04 00:03:23 -0800

    added article files folder

---
+++ article_files/template.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>UPark: USLS Parking</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<script src="../js/jquery.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="navbar navbar-fixed-top navbar-custom">
+				<div class="navbar-header">
+					<a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+				</div>
+				<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li><a class="navbar-brand" href="../index.php">U<span style="color: #009e3d;">PARK</span></a></li>
+						<li class="active link"><a href="../index.php">HOME</a></li>
+						<li class="link"><a href="about.php">ABOUT</a></li>
+						<li class="link"><a href="admin.php">ADMIN</a></li>
+						<li class="link"><a href="news.php">NEWS</a></li>
+						<li class="link"><a href="contact.php">CONTACT</a></li>
+						<li class="link"><a href="application_RN.php">APPLICATION</a></li>
+					</ul>
+				</div>
+			</div>
+			<!--Main Content-->
+			<div>
+			<ul class="page-content">
+				<li>
+					
+				</li>
+				<li>
+					<div class="greenbg text-center" style="margin-top:-50px;">
+						<p style="padding-top: 20px; font-size: 150%;">FOLLOW US ON</p>
+						<div class="row text-center">
+							<div class="col-md-2" style="padding:10px;"></div>
+							<div class="col-md-2" style="padding:10px;">
+								<strong style="font-size: 200%;">Facebook</strong><br>
+								<img src="../img/facebook.png" width="50px" height="50px" />
+							</div>
+							<div class="col-md-2" style="padding:10px;">
+								<strong style="font-size: 200%;">Twitter</strong><br>
+								<img src="../img/twitter.png" width="50px" height="50px" />
+							</div>
+							<div class="col-md-2" style="padding:10px;">
+								<strong style="font-size: 200%;">Google</strong><br>
+								<img src="../img/google.png" width="50px" height="50px" />
+							</div>
+							<div class="col-md-2" style="padding:10px;">
+								<strong style="font-size: 200%;">USLS</strong><br>
+								<img src="../img/google.png" width="50px" height="50px" />
+							</div>
+							<div class="col-md-2" style="padding:10px;"></div>
+						</div>
+						<ul class="spaced text-center inlinelist" style="padding-bottom: 20px; margin-top: 20px;">
+							<li class="inlineli"><a href="index.php" style="color: #fff;">HOME</a></li>
+							<li class="inlineli"><a href="index.php" style="color: #fff;">ABOUT</a></li>
+							<li class="inlineli"><a href="index.php" style="color: #fff;">ADMIN</a></li>
+							<li class="inlineli"><a href="index.php" style="color: #fff;">UPDATES</a></li>
+							<li class="inlineli"><a href="index.php" style="color: #fff;">NEWS</a></li>
+							<li class="inlineli"><a href="index.php" style="color: #fff;">CONTACT</a></li>
+							<li class="inlineli"><a href="index.php" style="color: #fff;">FORMS</a></li>
+						</ul>
+					</div>
+				</li>
+				<!--Footer-->
+				<li>
+					<div class="blackbg text-center" style="padding-top: 50px; padding-bottom: 10px;">
+						<p>Copyright &copy 2015<br>ALL RIGHTS RESERVED </p>
+					</div>
+				</li>
+			</ul>
+			</div>
+			<!--End of main content-->
+		</div>
+		<a href="#" class="scrollToTop text-center"><span class="glyphicon glyphicon-menu-up text-center" style="margin-top: 30%;"></span></a>
+	</body>
+	
+	<!--JQuery scripts-->
+	<script src="../js/stt.js"></script>
+</html>
